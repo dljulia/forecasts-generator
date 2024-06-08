b163ab205cabbe0b513c568287ed084aec07bac3
20ulia <uliatupik88@gmail.com>
2024-06-08 16:27:36 +0300

    Домашнее задание 5

--- script.js
+++ script.js
@@ -13,3 +13,61 @@
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const button = document.querySelector('.forecast-btn');
+const wrapPrediction = document.querySelector('.current-forecast');
+const prediction = wrapPrediction.querySelector('h1');
+const probability = wrapPrediction.querySelector('p');
+const containerPredictions = document.querySelector('.forecasts');
+const predictionTemplate = document.querySelector('#forecast-item');
+
+
+function generateNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+button.addEventListener('click', function() {
+  let predictionText = "";
+  const predictionNumber = generateNumber(1, 6);
+  const probabilityPrediction = generateNumber(0, 101);
+
+  switch (predictionNumber) {
+    case 1:
+      predictionText = "Тебя ждет повышение на работе!";
+      break;
+    case 2:
+      predictionText = "У тебя будет собака!";
+      break;
+    case 3:
+      predictionText = "Сегодня вечером ты пойдешь в кино!";
+      break;
+    case 4:
+      predictionText = "Все получится!";
+      break;
+    case 5:
+      predictionText = "В воскресенье ты пойдешь на свидание!";
+      break;
+  }
+
+  prediction.textContent = predictionText;
+  probability.textContent = `Вероятность: ${probabilityPrediction}%`;
+
+  const predictionCard = predictionTemplate.content.cloneNode(true);
+
+  predictionCard.querySelector('h3').textContent = prediction.textContent;
+  predictionCard.querySelector('p').textContent = probability.textContent;
+
+  containerPredictions.prepend(predictionCard);
+});
+
+
+
+
+
+
+
+
+
+
+
+
